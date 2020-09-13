4d589748933eddfaf35d04f2ce39146c7fc02e87
Ruslan Gadzhiev <gadzhievruslan@mail.ru>
2020-09-13 20:16:27 +0300

    categories in storybook finished

---
+++ src/admin/components/skillAddLine/skillAddLine.vue
@@ -0,0 +1,32 @@
+<template>    
+    <div 
+        :class="['skill-add-line-component', {blocked: blocked}]"
+    >
+        <div class="title">
+            <app-input />
+        </div>
+        <div class="percent">
+            <app-input type="number" min="0" max="100" maxlength="3" />
+        </div>
+        <iconed-button type="iconed" title="" />
+    </div>
+</template>
+
+<script>
+import input from "../input";
+import button from "../button";
+
+export default {
+    props: {
+        blocked: Boolean
+    },
+    components: {
+        appInput: input,
+        iconedButton: button
+    }
+}
+</script>
+
+<style lang="postcss" src="./skillAddLine.pcss" scoped>
+    
+</style>
